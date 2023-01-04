ece836da1131099947e7cefa231440041bbb0131
Hanna <anna_kon@bigmir.net>
2023-01-04 19:28:12 +0200

    1 task code refactoring

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,13 +5,27 @@ console.log(galleryItems);
 
 const galleryEl = document.querySelector('.gallery');
 
-const galleryItemsMarkup = galleryItems.map(galleryItem => `<div class="gallery__item">
-  <a class="gallery__link" href="${galleryItem.original}">
+// const galleryItemsMarkup = galleryItems.map(galleryItem => `<div class="gallery__item">
+//   <a class="gallery__link" href="${galleryItem.original}">
+//     <img
+//       class="gallery__image"
+//       src="${galleryItem.preview}"
+//       data-source="${galleryItem.original}"
+//       alt="${galleryItem.description}"
+//     />
+//   </a>
+// </div>`
+// ).join('');
+// galleryEl.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+const galleryItemsMarkup = galleryItems.map(({ original, preview, description }) => 
+  `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
-      src="${galleryItem.preview}"
-      data-source="${galleryItem.original}"
-      alt="${galleryItem.description}"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
     />
   </a>
 </div>`
@@ -20,28 +34,32 @@ galleryEl.insertAdjacentHTML("beforeend", galleryItemsMarkup);
 
 galleryEl.addEventListener("click", onImageClick);
 
+let imageInstance;
 
 function onImageClick(event) {
-    event.preventDefault();
-    if (event.target.nodeName !== "IMG") {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
     return;
-    }
+  }
     
-    const galleryOriginalImageEl = event.target.dataset.source;
-    console.log("🚀 ~ file: 01-gallery.js:31 ~ onImageClick ~ galleryOriginalImageEl", galleryOriginalImageEl);
-    const imageInstance = basicLightbox.create(`
+  const galleryOriginalImageEl = event.target.dataset.source;
+  console.log("🚀 ~ file: 01-gallery.js:31 ~ onImageClick ~ galleryOriginalImageEl", galleryOriginalImageEl);
+  imageInstance = basicLightbox.create(`
     <img src="${galleryOriginalImageEl}" width="800" height="600">
 `);
-    imageInstance.show();
-    galleryEl.addEventListener("keydown", onEscKeyPress);
-    function onEscKeyPress(event) {
-    if (event.code === "Escape") {
-      galleryEl.removeEventListener("keydown", onEscKeyPress);
-      imageInstance.close();
-    }
-  };
+  imageInstance.show();
+  galleryEl.addEventListener("keydown", onEscKeyPress);
+  
+}
 
+function onEscKeyPress(event) {
+  if (event.code === "Escape") {
+    galleryEl.removeEventListener("keydown", onEscKeyPress);
+    imageInstance.close();
+  }
 };
+
+
 
 
 
